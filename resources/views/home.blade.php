c0235a31e56fec9dc859c229d6998e90c4e95543
Đức Trần Mạnh <lovemai16102000@gmail.com>
2021-07-18 12:44:26 +0700

    frontend trang admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
+<!-- <head>
+    <link href="{{ asset('css/dasboard.css') }}" rel="stylesheet">
+</head> -->
+
 @section('content')
-<div class="container">
+
+<body>
+    <!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -12,12 +18,74 @@
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
+                    @endif -->
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa fa-user-o" aria-hidden="true"></i>
+                        </span>
+                        <span class="title">Adminghjhjghjg</span>
+                    </a>
+                </li>
+                
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa fa-home" aria-hidden="false"></i>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa fa-list" aria-hidden="true"></i>
+                        </span>
+                        <span class="title">Category</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa fa-book" aria-hidden="true"></i>
+                        </span>
+                        <span class="title">Book</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa fa-reply-all" aria-hidden="true"></i>
+                        </span>
+                        <span class="title">Borrow</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa fa-id-card" aria-hidden="true"></i>
+                        </span>
+                        <span class="title">Member</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><i class="fa fa-sign-out" aria-hidden="true"></i>
+                        </span>
+                        <span class="title">Sign out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="toggle" onclick="toggleMenu()"><i class="fa fa-bars" aria-hidden="true"></i>
         </div>
     </div>
-</div>
+
+    <script type="text/javascript">
+        function toggleMenu() {
+            let navigation = document.querySelector('.navigation');
+            let toggle = document.querySelector('.toggle');
+            navigation.classList.toggle('ative');
+            toggle.classList.toggle('ative');
+        }
+    </script>
+</body>
 @endsection
