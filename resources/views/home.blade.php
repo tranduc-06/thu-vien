b8ebcffdeb9db32b01cdc91f485e7a9d79041755
Đức Trần Mạnh <lovemai16102000@gmail.com>
2021-07-19 10:45:32 +0700

    them quan ly sach va danh muc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,6 @@
 
 @section('content')
 
-<body>
     <!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -20,72 +19,99 @@
                         </div>
                     @endif -->
 
-    <div class="container">
-        <div class="navigation">
-            <ul>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="fa fa-user-o" aria-hidden="true"></i>
-                        </span>
-                        <span class="title">Adminghjhjghjg</span>
-                    </a>
-                </li>
-                
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="fa fa-home" aria-hidden="false"></i>
-                        </span>
-                        <span class="title">Dashboard</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="fa fa-list" aria-hidden="true"></i>
-                        </span>
-                        <span class="title">Category</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="fa fa-book" aria-hidden="true"></i>
-                        </span>
-                        <span class="title">Book</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="fa fa-reply-all" aria-hidden="true"></i>
-                        </span>
-                        <span class="title">Borrow</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="fa fa-id-card" aria-hidden="true"></i>
-                        </span>
-                        <span class="title">Member</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><i class="fa fa-sign-out" aria-hidden="true"></i>
-                        </span>
-                        <span class="title">Sign out</span>
-                    </a>
-                </li>
-            </ul>
+    <body>
+        <div class="container1">
+            <div class="navigation">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <span class="icon"><i class="fa fa-user" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Admin</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="icon"><i class="fa fa-home" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" onclick="showcategory()">
+                            <span class="icon"><i class="fa fa-list" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Category</span>
+                            <span class="icon"><i class="fa fa-caret-down" aria-hidden="true"></i>
+                            </span>
+                        </a>
+                            <ul class="category-show">
+                                <li><a href="#"><span class="title">Add category</span></a></li>
+                                <li><a href="#"><span class="title">Show category</span></a></li>
+                            </ul>
+                        
+                    </li>
+                    <li>
+                        <a href="#" onclick="showbook()">
+                            <span class="icon"><i class="fa fa-book" aria-hidden="true"></i>
+                            </span>
+                            <span class="title btn-book">Book</span>
+                            <span class="icon"><i class="fa fa-caret-down" aria-hidden="true"></i>
+                            </span>
+                        </a>
+                        <ul class="book-show">
+                            <li><a href="#"><span class="title">Add Book</span></a></li>
+                            <li><a href="#"><span class="title">Show Book</span></a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="icon"><i class="fa fa-reply-all" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Borrow</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="icon"><i class="fa fa-id-card" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Member</span>
+                        </a>
+                    </li> 
+                    <li>
+                        <a href="#">
+                            <span class="icon"><i class="fa fa-sign-out" aria-hidden="true"></i>
+                            </span>
+                            <span class="title">Sign out</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="toggle" onclick="toggleMenu()"><i class="fa fa-bars" aria-hidden="true"></i>
+            </div>
         </div>
-        <div class="toggle" onclick="toggleMenu()"><i class="fa fa-bars" aria-hidden="true"></i>
-        </div>
-    </div>
 
-    <script type="text/javascript">
+        <script type="text/javascript">
         function toggleMenu() {
             let navigation = document.querySelector('.navigation');
             let toggle = document.querySelector('.toggle');
             navigation.classList.toggle('ative');
             toggle.classList.toggle('ative');
         }
-    </script>
-</body>
+
+        function showcategory()
+        {
+            let category = document.querySelector('.category-show');
+            category.classList.toggle('show');
+        
+        }
+        function showbook()
+        {
+            let book = document.querySelector('.book-show');
+            book.classList.toggle('show');
+        }
+
+        </script>
+    </body>
+    
 @endsection
